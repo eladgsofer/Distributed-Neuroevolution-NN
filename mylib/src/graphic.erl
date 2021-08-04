a24809f92bdbdfd0b96c6d98ae7f468abbfd29e2
TomKessous <tomkissos@gmail.com>
2021-08-04 11:38:13 +0300

    Working Mutator + gui

---
+++ mylib/src/graphic.erl
@@ -0,0 +1,182 @@
+-module(graphic).
+-author("Tom").
+
+-behaviour(wx_object).
+-include_lib("wx/include/wx.hrl").
+
+%% API
+-export([start/0, update_location/3]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, handle_event/2, terminate/2,
+  code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+-record(main_PC_state, {frame, panel, sensor_img}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% @doc Spawns the server and registers the local name (unique)
+-spec(start() ->
+  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+start() ->
+  wx_object:start({local, ?SERVER}, ?MODULE, [], []).
+
+update_location(Rabbit_pos,Hunter_pos,Statistics) ->
+  wx_object:call(?SERVER, {update_img, Rabbit_pos,Hunter_pos,Statistics}).
+
+
+%%%===================================================================
+%%% wx_object callbacks
+%%%===================================================================
+
+%% @private
+%% @doc Initializes the server
+%-spec(init(Args :: term()) ->
+%  {ok, State :: #main_PC_state{}} | {ok, State :: #main_PC_state{}, timeout() | hibernate} |
+%  {stop, Reason :: term()} | ignore).
+init([]) ->
+  ets:new(db,[set,public,named_table]),
+  ets:new(stats,[set,public,named_table]),
+  ets:insert(stats,[{process, 0},{fitness, 0}, {hunter_location, {20,20}}, {rabbit_location, {10,10}}]),
+  %ets:insert(sensor_states,[]),
+  %Graphics
+  WxServer = wx:new(),
+  Frame = wxFrame:new(WxServer, ?wxID_ANY, "Neuroevolution Neural Network", [{size,{1280, 720}}]),
+  Panel  = wxPanel:new(Frame),
+  wxFrame:createStatusBar(Frame),
+  wxFrame:show(Frame),
+  BackGround = wxBitmap:new("background.bmp"),
+  Rabbit = wxBitmap:new("rabbit.bmp"),
+  Hunter = wxBitmap:new("hunter.bmp"),
+  ets:insert(db,{{10,10},Rabbit}),
+  ets:insert(db,{{20,20},Hunter}),
+  CallBackPaint =	fun(#wx{event = #wxPaint{}}, _wxObj)->
+    Paint = wxBufferedPaintDC:new(Panel),
+    wxDC:drawBitmap(Paint,BackGround,{0,0}),
+    Text = lists:flatten(io_lib:format("Processes: ~p,  Fitness: ~p,  Hunter: ~p,  Rabbit: ~p",
+      [ets:lookup_element(stats, process	 , 2),
+        ets:lookup_element(stats, fitness , 2),
+        ets:lookup_element(stats, hunter_location	 , 2),
+        ets:lookup_element(stats, rabbit_location	 , 2)])),
+    % Updating the status bar with the game info
+    wxFrame:setStatusText(Frame, Text),
+    drawETS(Paint),
+    wxBufferedPaintDC:destroy(Paint) end,
+
+  % connect panel
+  wxFrame:connect(Panel, paint, [{callback, CallBackPaint}]),
+
+  % Self messaging timer
+  timer:send_interval(80, self(), graphicUpdate),
+
+  %sensor:start(sensor1), %ToDo: temp
+  {Frame, #main_PC_state{frame = Frame, panel = Panel}}.
+
+%% @private
+%% @doc Handling call messages
+-spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
+    State :: #main_PC_state{}) ->
+  {reply, Reply :: term(), NewState :: #main_PC_state{}} |
+  {reply, Reply :: term(), NewState :: #main_PC_state{}, timeout() | hibernate} |
+  {noreply, NewState :: #main_PC_state{}} |
+  {noreply, NewState :: #main_PC_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), Reply :: term(), NewState :: #main_PC_state{}} |
+  {stop, Reason :: term(), NewState :: #main_PC_state{}}).
+handle_call({update_img, Rabbit_pos,Hunter_pos,Statistics}, _From, State = #main_PC_state{ panel = Panel}) ->
+  ets:delete_all_objects(ets:whereis(db)),
+  RabbitBMP = wxBitmap:new("rabbit.bmp"),
+  HunterBMP = wxBitmap:new("hunter.bmp"),
+  ets:insert(db,{Rabbit_pos,RabbitBMP}),ets:insert(db,{Hunter_pos,HunterBMP}),
+  ets:insert(stats,Statistics++[{hunter_location, Hunter_pos}, {rabbit_location, Rabbit_pos}]),   %%Statistics=[{process, #num},{fitness, $num}, {hunter_location, {x,y}}, {rabbit_location, {x,y}}]
+  {reply, ok, State};
+handle_call(_Request, _From, State = #main_PC_state{}) ->
+  {reply, ok, State}.
+
+%% @private
+%% @doc Handling cast messages
+-spec(handle_cast(Request :: term(), State :: #main_PC_state{}) ->
+  {noreply, NewState :: #main_PC_state{}} |
+  {noreply, NewState :: #main_PC_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #main_PC_state{}}).
+handle_cast({update_img, {Sensor_Pos,Sensor_State}}, State = #main_PC_state{ panel = Panel}) ->
+  State_img = case Sensor_State of
+    active -> "active_sensor.bmp";
+    sending -> "sensor_sending_data.bmp";
+    asleep -> "sensor_sleep.bmp"
+  end,
+  case ets:member(db,Sensor_Pos) of
+    true ->
+      [{Sensor_Pos,SensorBMP_prev}] = ets:lookup(db,Sensor_Pos),
+      wxBitmap:destroy(SensorBMP_prev);
+    false -> ok
+  end,
+  SensorBMP = wxBitmap:new(State_img),
+  ets:insert(db,{Sensor_Pos,SensorBMP}),
+  {noreply, State};
+handle_cast(_Request, State = #main_PC_state{}) ->
+  {noreply, State}.
+
+%% @private
+%% @doc Handling all non call/cast messages
+-spec(handle_info(Info :: timeout() | term(), State :: #main_PC_state{}) ->
+  {noreply, NewState :: #main_PC_state{}} |
+  {noreply, NewState :: #main_PC_state{}, timeout() | hibernate} |
+  {stop, Reason :: term(), NewState :: #main_PC_state{}}).
+handle_info(_Info, State = #main_PC_state{}) ->
+  wxWindow:refresh(State#main_PC_state.panel,[{eraseBackground,false}]),{noreply, State},
+  {noreply, State}.
+
+handle_event(#wx{event = #wxClose{}},State = #main_PC_state {frame = Frame}) -> % close window event
+  io:format("Exiting\n"),
+  wxWindow:destroy(Frame),
+  wx:destroy(),
+  {stop,normal,State};
+
+handle_event(_Event,State) -> % when left click has been pressed, activate navigation function
+  {noreply,State}.
+
+
+%% @private
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+-spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
+    State :: #main_PC_state{}) -> term()).
+terminate(_Reason, _State = #main_PC_state{}) ->
+  ok.
+
+%% @private
+%% @doc Convert process state when code is changed
+-spec(code_change(OldVsn :: term() | {down, term()}, State :: #main_PC_state{},
+    Extra :: term()) ->
+  {ok, NewState :: #main_PC_state{}} | {error, Reason :: term()}).
+code_change(_OldVsn, State = #main_PC_state{}, _Extra) ->
+  {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+% ===== iterates all ets elements and update image =====
+drawETS(Painter) ->
+  First = ets:first(db),
+  case First of
+    '$end_of_table'-> ok;
+    _ ->
+      [{Pos,BMP}] = ets:lookup(db,First),
+      wxDC:drawBitmap(Painter, BMP, Pos),
+      drawETS(Painter,First)
+  end.
+drawETS(Painter,Curr) ->
+  Next = ets:next(db,Curr),
+  case Next of
+    '$end_of_table' -> ok;
+    _ ->
+      [{Pos,BMP}] = ets:lookup(db,Next),
+      wxDC:drawBitmap(Painter, BMP, Pos),
+      drawETS(Painter,Next)
+  end.
--- mylib/src/mutate.erl
+++ mylib/src/mutate.erl
@@ -23,15 +23,15 @@ mutate(G)->
 mutate(T,0,_)-> [Head|Tail] = ets:tab2list(T),L = lists:reverse(Tail),
   New_Genotype = [hd(L)]++[Head]++tl(L),New_Genotype;
 mutate(T,N,Cx)->
-  io:format("Iteration:~p~n",[N]),
-  X=rand:uniform(6),
+  X = rand:uniform(7),
   case X of
-    1->setBias(T,N,Cx);
-    2->removeBias(T,N,Cx);
-    3->addEdge(T,N,Cx);
-    4->addNeuron(T,N,Cx);
-    5->removeEdge(T,N,Cx);
-    6->changeWeight(T,N,Cx)
+    1->io:format("setBias:~n"), setBias(T,N,Cx);
+    2->io:format("removeBias:~n"),removeBias(T,N,Cx);
+    3->io:format("addEdge:~n"),addEdge(T,N,Cx);
+    4->io:format("addNeuron:~n"),addNeuron(T,N,Cx);
+    5->io:format("removeEdge:~n"),removeEdge(T,N,Cx);
+    6->io:format("changeWeight:~n"),changeWeight(T,N,Cx);
+    7-> io:format("removeNeuron:~n"),removeNeuron(T,N,Cx)
   end.
 
 setBias(T,N,Cx)-> Nids = Cx#cortex.nids, I=rand:uniform(length(Nids)), Id_chosen = lists:nth(I, Nids),
@@ -49,12 +49,12 @@ removeBias(T,N,Cx)-> Nids = Cx#cortex.nids, I=rand:uniform(length(Nids)), Id_cho
 addEdge(T,N,Cx)-> From_list = Cx#cortex.sensor_ids++Cx#cortex.nids,
   {Name_from,Id_from} = lists:nth(rand:uniform(length(From_list)), From_list),
   case Name_from of
-    neuron->{Layer,_} = Id_from, To_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Cx#cortex.nids,L >= Layer]++Cx#cortex.actuator_ids;
-    sensor-> To_list = Cx#cortex.nids++Cx#cortex.actuator_ids
+    neuron->{Layer,_} = Id_from, To_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Cx#cortex.nids,L >= Layer];
+    sensor-> To_list = Cx#cortex.nids
   end,
   {Name_to,Id_to} = lists:nth(rand:uniform(length(To_list)), To_list),
   Tab = addEdge(T,Name_from,Id_from,Name_to,Id_to,from),case Tab =:= exist of
-                                                          true -> mutate(T,N-1,Cx);
+                                                          true -> mutate(T,N,Cx);
                                                           false-> New_T = addEdge(Tab,Name_from,Id_from,Name_to,Id_to,to),mutate(New_T,N-1,Cx)
                                                         end.
 
@@ -73,23 +73,26 @@ addEdge(T,sensor,Id_from,Name_to,Id_to,from)-> From_table = hd(ets:select(T, [{#
                                          end;
 addEdge(Tab,Name_from,Id_from,neuron,Id_to,to)->
   To_table = hd(ets:select(Tab,[{#neuron{id={neuron,Id_to}, cx_id='_', af='_', input_idps = '_',output_ids='_'}, [], ['$_']}])),
-  Old = To_table#neuron.input_idps,New = [{{Name_from,Id_from},[rand:uniform()-0.5]}]++Old,U_N = To_table#neuron{input_idps=New},
-  ets:delete_object(Tab,To_table), ets:insert(Tab,U_N), Tab;
-
-addEdge(Tab,Name_from,Id_from,actuator,Id_to,to)->
-  To_table = hd(ets:select(Tab, [{#actuator{id={actuator,Id_to}, cx_id='_', name='_', vl = '_',fanin_ids='_'}, [], ['$_']}])),
-  Old = To_table#actuator.fanin_ids,New = [{Name_from,Id_from}]++Old,U_A = To_table#actuator{fanin_ids=New},
-  ets:delete_object(Tab,To_table), ets:insert(Tab,U_A), Tab.
-
-removeEdge(T,N,Cx)-> From_list = Cx#cortex.sensor_ids++Cx#cortex.nids,
-  {Name_from,Id_from} = lists:nth(rand:uniform(length(From_list)), From_list),
+  Old = To_table#neuron.input_idps,
   case Name_from of
-    neuron->{Layer,_} = Id_from, To_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Cx#cortex.nids,L >= Layer]++Cx#cortex.actuator_ids;
-    sensor-> To_list = Cx#cortex.nids++Cx#cortex.actuator_ids
+    sensor ->New = [{{Name_from,Id_from},[rand:uniform()-0.5,rand:uniform()-0.5,rand:uniform()-0.5,rand:uniform()-0.5]}]++Old;
+    neuron-> New = [{{Name_from,Id_from},[rand:uniform()-0.5]}]++Old
   end,
+  U_N = To_table#neuron{input_idps=New},
+  ets:delete_object(Tab,To_table), ets:insert(Tab,U_N), Tab.
+
+%addEdge(Tab,Name_from,Id_from,actuator,Id_to,to)->
+ % To_table = hd(ets:select(Tab, [{#actuator{id={actuator,Id_to}, cx_id='_', name='_', vl = '_',fanin_ids='_'}, [], ['$_']}])),
+ % Old = To_table#actuator.fanin_ids,New = [{Name_from,Id_from}]++Old,U_A = To_table#actuator{fanin_ids=New},
+ % ets:delete_object(Tab,To_table), ets:insert(Tab,U_A), Tab.
+
+removeEdge(T,N,Cx)-> Final_layer = lists:max([L||{_,{L,_}} <-Cx#cortex.nids]),
+  From_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Cx#cortex.nids,L<Final_layer],
+  {Name_from,Id_from} = lists:nth(rand:uniform(length(From_list)), From_list),
+  {Layer,_} = Id_from, To_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Cx#cortex.nids,L >= Layer],
   {Name_to,Id_to} = lists:nth(rand:uniform(length(To_list)), To_list),Tab = removeEdge(T,Name_from,Id_from,Name_to,Id_to,from),
   case Tab =:= dont_exist of
-    true -> mutate(T,N-1,Cx);
+    true -> mutate(T,N,Cx);
     false-> New_T = removeEdge(Tab,Name_from,Id_from,Name_to,Id_to,to), mutate(New_T,N-1,Cx)
   end.
 
@@ -101,21 +104,21 @@ removeEdge(T,neuron,Id_from,Name_to,Id_to,from)->From_table = hd(ets:select(T, [
                                            false-> dont_exist
                                          end;
 
-removeEdge(T,sensor,Id_from,Name_to,Id_to,from)-> From_table = hd(ets:select(T, [{#sensor{id={sensor,Id_from}, cx_id='_', name='_', vl = '_',fanout_ids='_'}, [], ['$_']}])),
-  Fanout = From_table#sensor.fanout_ids, case lists:member({Name_to,Id_to},Fanout) of
-                                           true-> U_S = From_table#sensor{fanout_ids = lists:delete({Name_to,Id_to},Fanout)},
-                                             ets:delete_object(T,From_table), ets:insert(T,U_S),T;
-                                           false -> dont_exist
-                                         end;
+%removeEdge(T,sensor,Id_from,Name_to,Id_to,from)-> From_table = hd(ets:select(T, [{#sensor{id={sensor,Id_from}, cx_id='_', name='_', vl = '_',fanout_ids='_'}, [], ['$_']}])),
+ % Fanout = From_table#sensor.fanout_ids, case lists:member({Name_to,Id_to},Fanout) of
+  %                                         true-> U_S = From_table#sensor{fanout_ids = lists:delete({Name_to,Id_to},Fanout)},
+   %                                          ets:delete_object(T,From_table), ets:insert(T,U_S),T;
+    %                                       false -> dont_exist
+     %                                    end;
 removeEdge(Tab,Name_from,Id_from,neuron,Id_to,to)->
   To_table = hd(ets:select(Tab,[{#neuron{id={neuron,Id_to}, cx_id='_', af='_', input_idps = '_',output_ids='_'}, [], ['$_']}])),
   Old = To_table#neuron.input_idps,New = lists:keydelete({Name_from,Id_from},1,Old),U_N = To_table#neuron{input_idps=New},
-  ets:delete_object(Tab,To_table), ets:insert(Tab,U_N), Tab;
+  ets:delete_object(Tab,To_table), ets:insert(Tab,U_N), Tab.
 
-removeEdge(Tab,Name_from,Id_from,actuator,Id_to,to)->
-  To_table = hd(ets:select(Tab, [{#actuator{id={actuator,Id_to}, cx_id='_', name='_', vl = '_',fanin_ids='_'}, [], ['$_']}])),
-  Old = To_table#actuator.fanin_ids,New = lists:delete({Name_from,Id_from},Old),U_A = To_table#actuator{fanin_ids=New},
-  ets:delete_object(Tab,To_table), ets:insert(Tab,U_A), Tab.
+%removeEdge(Tab,Name_from,Id_from,actuator,Id_to,to)->
+ % To_table = hd(ets:select(Tab, [{#actuator{id={actuator,Id_to}, cx_id='_', name='_', vl = '_',fanin_ids='_'}, [], ['$_']}])),
+  %Old = To_table#actuator.fanin_ids,New = lists:delete({Name_from,Id_from},Old),U_A = To_table#actuator{fanin_ids=New},
+  %ets:delete_object(Tab,To_table), ets:insert(Tab,U_A), Tab.
 
 changeWeight(T,N,Cx)-> Nids = Cx#cortex.nids, I=rand:uniform(length(Nids)), Id_chosen = lists:nth(I, Nids),
   Neuron = hd(ets:select(T, [{#neuron{id=Id_chosen, cx_id='_', af='_', input_idps = '_',output_ids='_'}, [], ['$_']}])),
@@ -124,14 +127,38 @@ changeWeight(T,N,Cx)-> Nids = Cx#cortex.nids, I=rand:uniform(length(Nids)), Id_c
   N_Input_idps = lists:sublist(Input_idps,I1-1)++[{Id,N_W}]++lists:nthtail(I1,Input_idps),
   U_N = Neuron#neuron{input_idps = N_Input_idps},ets:delete_object(T,Neuron), ets:insert(T,U_N),mutate(T,N-1,Cx).
 
-addNeuron(T,N,Cx)-> Old = Cx#cortex.nids, Layer = rand:uniform(lists:max([L||{_,{L,_}} <- Old])) ,
-  Neuron = #neuron{
-  id ={neuron,{Layer,constructor:generate_id()}},
-  cx_id = Cx#cortex.id,
-  af = tanh,
-  input_idps=[],
-  output_ids=[]},
-  ets:insert(T,Neuron),
-  New_Cx = Cx#cortex{nids = Old++[Neuron#neuron.id]}, ets:delete_object(T,Cx), ets:insert(T,New_Cx),
-  ets:insert(T,Neuron),mutate(T,N-1,New_Cx).
+addNeuron(T,N,Cx)-> Old = Cx#cortex.nids, Layer = rand:uniform(lists:max([L||{_,{L,_}} <- Old])-1),
+  To_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Old,L > Layer], {Name_to,Id_to} = lists:nth(rand:uniform(length(To_list)),To_list),
+  From_list = [{Name,{L,Id}}||{Name,{L,Id}} <-Old,L < Layer]++Cx#cortex.sensor_ids, {Name_from,Id_from} = lists:nth(rand:uniform(length(From_list)),From_list),
+  N_id = constructor:generate_id(),
+  Neuron = #neuron{id ={neuron,{Layer,N_id}},cx_id = Cx#cortex.id,af = tanh, input_idps=[{bias,rand:uniform()-0.5}],output_ids=[]},
+  ets:insert(T,Neuron), New_Cx = Cx#cortex{nids = [Neuron#neuron.id]++Old}, ets:delete_object(T,Cx), ets:insert(T,New_Cx),
+  Tab = addEdge(T,Name_from,Id_from,neuron,{Layer,N_id},from),Tab1=addEdge(Tab,Name_from,Id_from,neuron,{Layer,N_id},to),
+  Tab2 = addEdge(Tab1,neuron,{Layer,N_id},Name_to,Id_to,from),Tab3=addEdge(Tab2,neuron,{Layer,N_id},Name_to,Id_to,to),mutate(Tab3,N-1,New_Cx).
+
+removeNeuron(T,N,Cx)-> Layers = [L||{_,{L,_}} <-Cx#cortex.nids], Final_layer = lists:max(Layers),First_layer=lists:min(Layers),
+  Middle_neurons=[{Name,{L,ID}}||{Name,{L,ID}} <-Cx#cortex.nids,First_layer<L,L<Final_layer],
+  Id_chosen = lists:nth(rand:uniform(length(Middle_neurons)),Middle_neurons),
+  Chosen_Neuron = hd(ets:select(T, [{#neuron{id=Id_chosen, cx_id='_', af='_', input_idps = '_',output_ids='_'}, [], ['$_']}])),
+  Inputs_tmp = [Id||{Id,_} <- Chosen_Neuron#neuron.input_idps], Inputs = lists:sublist(Inputs_tmp,length(Inputs_tmp)-1),
+  Outputs=Chosen_Neuron#neuron.output_ids, Tab =removeNeuronEdges(T,Inputs,Outputs,Chosen_Neuron#neuron.id),
+  New_Cx = Cx#cortex{nids = lists:delete(Chosen_Neuron#neuron.id,Cx#cortex.nids)},ets:delete_object(Tab,Cx),ets:insert(Tab,New_Cx),
+  Del = hd(ets:select(Tab, [{#neuron{id=Id_chosen, cx_id='_', af='_', input_idps = '_',output_ids='_'}, [], ['$_']}])),
+  ets:delete_object(Tab,Del),mutate(Tab,N-1,New_Cx).
+
+removeNeuronEdges(T,[],[],_)->T;
+removeNeuronEdges(T,[{Name_from,Id_from}|Inputs],[],{Chosen_name,Chosen_Id})->
+  T1 = removeEdge(T,Name_from,Id_from,Chosen_name,Chosen_Id,from),
+  T2= removeEdge(T1,Name_from,Id_from,Chosen_name,Chosen_Id,to),
+  removeNeuronEdges(T2,Inputs,[],{Chosen_name,Chosen_Id});
+removeNeuronEdges(T,[],[{Name_to,Id_to}|Outputs],{Chosen_name,Chosen_Id})->
+  T1 = removeEdge(T,Chosen_name,Chosen_Id,Name_to,Id_to,from),
+  T2= removeEdge(T1,Chosen_name,Chosen_Id,Name_to,Id_to,to),
+  removeNeuronEdges(T2,[],Outputs,{Chosen_name,Chosen_Id});
+removeNeuronEdges(T,[{Name_from,Id_from}|Inputs],[{Name_to,Id_to}|Outputs],{Chosen_name,Chosen_Id})->
+  T1 = removeEdge(T,Name_from,Id_from,Chosen_name,Chosen_Id,from),
+  T2= removeEdge(T1,Name_from,Id_from,Chosen_name,Chosen_Id,to),
+  T3 = removeEdge(T2,Chosen_name,Chosen_Id,Name_to,Id_to,from),
+  T4= removeEdge(T3,Chosen_name,Chosen_Id,Name_to,Id_to,to),
+  removeNeuronEdges(T4,Inputs,Outputs,{Chosen_name,Chosen_Id}).
 
