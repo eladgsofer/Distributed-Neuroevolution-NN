704be3f61a916cc395eece89f2ee19c72beea31c
TomKessous <tomkissos@gmail.com>
2021-08-05 17:36:43 +0300

    DataBase

--- mylib/src/db.erl
+++ mylib/src/db.erl
@@ -12,17 +12,17 @@
 %% API
 -export([init/1,write/5,read_all_mutateIter/1]).
 
-init([Node_List]) ->
+init(Node_List) ->
   mnesia:create_schema([node()|Node_List]), mnesia:start(),
-  mnesia:create_table(db,[{ram_copies, [node()|Node_List]},{attributes, record_info(fields,nn_rec)}]).
+  mnesia:create_table(db,[{ram_copies, [node()|Node_List]},{attributes, record_info(fields,db)}]).
 
 write(NN_id, MutId, Gene, Processes_count, Score) ->
-  Tmp = #nn_rec{nn_id = NN_id,mutId = MutId,gene = Gene,processes_count = Processes_count,score = Score},
+  Tmp = #db{nn_id = NN_id,mutId = MutId,gene = Gene,processes_count = Processes_count,score = Score},
   Fun = fun() ->mnesia:write(Tmp) end, mnesia:transaction(Fun).
 
 read_all_mutateIter(Iter) ->
   F = fun() ->
-    Elem = #nn_rec{mutId = Iter, score = '$1', gene = '$2', _ = '_'},
+    Elem = #db{mutId = Iter, score = '$1', gene = '$2', _ = '_'},
     mnesia:select(db, [{Elem, [], ['$$']}])
       end,
   mnesia:transaction(F).
