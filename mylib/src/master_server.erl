8a0f35f942021898fdbd5824f0e25e7e50a49318
Elad Sofer <elad.g.sofer@gmail.com>
2021-08-10 16:52:51 +0300

    dist check

--- mylib/src/master_server.erl
+++ mylib/src/master_server.erl
@@ -30,9 +30,9 @@
 
 -define(ELAD, 'king@pds-MacBook-Pro').
 
--define(MASTER_NODE, ?TOM).
+-define(MASTER_NODE, 'king@132.72.104.105').
 
--define(NODE1, 'node1@Tom-VirtualBox').
+-define(NODE1, 'node1@132.72.104.105').
 -define(NODE2, 'node2@Tom-VirtualBox').
 -define(NODE3, 'node3@Tom-VirtualBox').
 -define(TIMER_INTERVAL, 1000).
@@ -159,6 +159,7 @@ handle_info(_Info, State) ->
                true->
                  handleIteration(State, Active_Nodes, Mutate_iteration);
                false ->
+                 io:format("#### NODES CHANGED ACTIVE:~p PREV:~p~n", [Active_Nodes, State#state.prev_nodes]),
                  % Update the work per node equally
                  Updated_State_1 = State#state{prev_nodes = Active_Nodes, nnPerNode = round(math:floor(NN_Amount/length(Active_Nodes)))},
                  io:format("NEW ACTIVE NODES:~p~n", [Active_Nodes]),
@@ -275,11 +276,11 @@ generateSeeds(NN_amount,Layers)-> % Initialize State
 
 findActiveNodes()->
   NodeList = [?MASTER_NODE, ?NODE1, ?NODE2, ?NODE3],
-  [Node || Node<- NodeList, net_adm:ping(Node)==pong].
+  [Node || Node<- NodeList, net_kernel:connect_node(Node)==true].
 
 findSlaves()->
   NodeList = [?NODE1, ?NODE2, ?NODE3],
-  [Node || Node<- NodeList, net_adm:ping(Node)==pong].
+  [Node || Node<- NodeList, net_kernel:connect_node(Node)==true].
 
 restartIteration(State, MutIter, ActiveNodes)->
   database:delete_all_mutateIter(MutIter),
