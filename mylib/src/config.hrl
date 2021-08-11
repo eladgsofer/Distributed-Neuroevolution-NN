1d69abbf975e07cdfaa35be295ebf945bcbc263b
Elad Sofer <elad.g.sofer@gmail.com>
2021-08-11 11:14:07 +0300

    config file

---
+++ mylib/src/config.hrl
@@ -0,0 +1,22 @@
+%%%-------------------------------------------------------------------
+%%% @author elad.sofer
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 11. Aug 2021 11:03 AM
+%%%-------------------------------------------------------------------
+-author("elad.sofer").
+
+% Systems properties
+-define(SIM_ITERATIONS, 62).
+-define(HUNTER_INIT_LOC, [1,40]).
+
+% Nodes
+-define(TOM, 'king@Tom-VirtualBox').
+-define(ELAD, 'king@pds-MacBook-Pro').
+
+-define(MASTER_NODE, 'king@132.72.104.105').
+-define(NODE1, 'node1@132.72.104.125').
+-define(NODE2, 'node2@132.72.104.171').
+-define(NODE3, 'node3@132.72.104.248').
